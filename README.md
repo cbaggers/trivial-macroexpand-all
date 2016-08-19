a28963efb0ebbfefb04ea43b9fa31caad47a61b1
Baggers <chris.bagley@gmail.com>
2016-08-19 07:29:26 +0200

    first drop

---
+++ README.md
@@ -0,0 +1,11 @@
+# trivial-macroexpand-all
+
+Provides a macroexpand-all function that calls the implementation specific equivalent.
+
+Supports: `abcl`, `allegro`, `ccl`, `clisp`, `cmucl`, `corman`, `lispworks`, `mkcl`, `sbcl` & `scl`
+
+On unsupported implementations it returns the form unchanged and emits a warning.
+
+## Trivial..again?
+
+Yup another library using the `trivial-*` naming convention`, `but look at the source.. it's pretty damn trivial
---
+++ trivial-macroexpand-all.lisp
@@ -0,0 +1,56 @@
+(in-package #:trivial-macroexpand-all)
+
+#+abcl
+(defun macroexpand-all (form &optional env)
+  (ext:macroexpand-all form env))
+
+#+allegro
+(defun macroexpand-all (form &optional env)
+  (declare (ignore env))
+  #+(and allegro-version>= (version>= 8 0))
+  (excl::walk-form form)
+  #-(and allegro-version>= (version>= 8 0))
+  (excl::walk form))
+
+#+ccl
+(defun macroexpand-all (form &optional env)
+  (ccl:macroexpand-all form env))
+
+#+clisp
+(defun macroexpand-all (form &optional env)
+  (declare (ignore env))
+  (ext:expand-form form))
+
+#+cmucl
+(defun macroexpand-all (form &optional env)
+  (walker:macroexpand-all form env))
+
+#+corman
+(defun macroexpand-all (form &optional env)
+  (declare (ignore env))
+  (ccl:macroexpand-all form))
+
+#+lispworks
+(defun macroexpand-all (form &optional env)
+  (declare (ignore env))
+  (walker:walk-form form))
+
+#+mkcl
+(defun macroexpand-all (form &optional env)
+  (declare (ignore env))
+  (walker:macroexpand-all form))
+
+#+sbcl
+(defun macroexpand-all (form &optional env)
+  (sb-cltl2:macroexpand-all form env))
+
+#+scl
+(defun macroexpand-all (form &optional env)
+  (declare (ignore env))
+  (macroexpand form))
+
+#-(or abcl allegro ccl clisp cmucl corman lispworks mkcl sbcl scl)
+(defun macroexpand-all (form &optional env)
+  (declare (ignore env))
+  (warn "trivial-macroexpand-all is not currently available for this implementation")
+  form)
